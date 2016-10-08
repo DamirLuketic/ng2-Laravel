a3dc82257861ab32c2dd0818ea1cec551eef3b35
Damir <luketic.damir@gmail.com>
2016-10-08 14:22:22 +0200

    before add emit

---
+++ app/tasks/task-detail/task-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ts-task-detail',
+  template: `
+    <p>
+      task-detail Works!
+    </p>
+  `,
+  styles: []
+})
+export class TaskDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
